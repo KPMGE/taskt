36f30a28195974184a7501b02e0e6c65f05ea916
KPMGE <jesuskevin254@gmail.com>
2023-11-26 21:17:07 -0300

    :recycle: create home screen

--- app/src/main/java/com/example/taskt/MainActivity.kt
+++ app/src/main/java/com/example/taskt/MainActivity.kt
@@ -7,20 +7,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.taskt.ui.HomeScreen
 import com.example.taskt.ui.theme.TasktTheme
-import com.example.taskt.ui.todo_list.TodoListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TasktTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TodoListScreen()
+                    HomeScreen()
                 }
             }
         }
