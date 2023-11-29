d74b7a621c1a69905b387df133575a7195770ee2
KPMGE <jesuskevin254@gmail.com>
2023-11-28 22:11:58 -0300

    :lipstick: implement new checkox on todo list

--- app/src/main/java/com/example/taskt/ui/todo_list/TodoCard.kt
+++ app/src/main/java/com/example/taskt/ui/todo_list/TodoCard.kt
@@ -1,20 +1,29 @@
 package com.example.taskt.ui.todo_list
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Checkbox
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import com.example.taskt.data.Todo
+import com.example.taskt.ui.theme.Blue900
+import com.example.taskt.widgets.RoundedCheckbox
 
 @Composable
 fun TodoCard(todo: Todo) {
@@ -28,17 +37,32 @@ fun TodoCard(todo: Todo) {
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
             var checked = remember { mutableStateOf(false) }
 
-            Checkbox(
-                checked = checked.value,
-                onCheckedChange = { checked.value = it }
-            )
-            Text(
-                text = todo.title,
-                textAlign = TextAlign.Center,
-                color = Color.DarkGray
+            Row(
+                modifier = Modifier.weight(0.8f),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                RoundedCheckbox(
+                    isChecked = checked.value,
+                    checkedColor = Blue900,
+                    onValueChange = { checked.value = it }
+                )
+                Text(
+                    modifier = Modifier.padding(start = 9.dp),
+                    text = todo.title,
+                    textDecoration = if (checked.value) TextDecoration.LineThrough else null,
+                    color = if (checked.value) Color.LightGray else Color.DarkGray
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .size(10.dp)
+                    .clip(CircleShape)
+                    .background(Color.Red)
             )
         }
     }
