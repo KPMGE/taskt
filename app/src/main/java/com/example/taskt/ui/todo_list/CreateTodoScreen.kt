c1f9dfd2fbc9bad9987a399efef183fbfa55330d
KPMGE <jesuskevin254@gmail.com>
2023-11-30 23:04:38 -0300

    :sparkles: implement cancel and done buttons on create todo screen

--- app/src/main/java/com/example/taskt/ui/todo_list/CreateTodoScreen.kt
+++ app/src/main/java/com/example/taskt/ui/todo_list/CreateTodoScreen.kt
@@ -1,9 +1,31 @@
 package com.example.taskt.ui.todo_list
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CreateTodoScreen() {
-    Text("Create screen")
+    Column (
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "cancel", color = Color(0xFFC70039))
+            Text(text = "done", color = Color(0xFF0096FF))
+        }
+    }
 }
