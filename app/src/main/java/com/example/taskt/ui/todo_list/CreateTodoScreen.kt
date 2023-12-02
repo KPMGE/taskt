df55de0059e3e694efc9a9d5e9ae912b2d6c42a8
KPMGE <jesuskevin254@gmail.com>
2023-12-02 19:40:42 -0300

    :sparkles: implement add todo text field

--- app/src/main/java/com/example/taskt/ui/todo_list/CreateTodoScreen.kt
+++ app/src/main/java/com/example/taskt/ui/todo_list/CreateTodoScreen.kt
@@ -5,16 +5,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateTodoScreen() {
+    var text by remember { mutableStateOf("") }
+
     Column (
         modifier = Modifier.fillMaxSize()
     ) {
@@ -27,5 +37,22 @@ fun CreateTodoScreen() {
             Text(text = "cancel", color = Color(0xFFC70039))
             Text(text = "done", color = Color(0xFF0096FF))
         }
+
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text(text = "I need to...") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                focusedTextColor = Color.DarkGray,
+                disabledTextColor = Color.Transparent,
+                containerColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            )
+        )
     }
 }
