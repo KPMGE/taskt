75ffda66ca253057f9846cb65122391ca3bbe11e
KPMGE <jesuskevin254@gmail.com>
2024-05-03 00:05:47 -0300

    :sparkles: add preview to todo list

--- app/src/main/java/com/example/taskt/ui/todo_list/TodoList.kt
+++ app/src/main/java/com/example/taskt/ui/todo_list/TodoList.kt
@@ -9,8 +9,10 @@ import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.taskt.data.Todo
+import com.example.taskt.ui.theme.TasktTheme
 
 @Composable
 fun TodosList(todos: List<Todo>, modifier: Modifier = Modifier) {
@@ -36,5 +38,29 @@ fun TodosList(todos: List<Todo>, modifier: Modifier = Modifier) {
                 color = Color.LightGray
             )
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TodoListPreview() {
+    TasktTheme {
+        val todos = listOf(
+            Todo(
+                id = 1,
+                title = "Todo 1",
+                description = "Todo 1 description",
+                done = false,
+                hexColor = "#0000FF",
+            ),
+            Todo(
+                id = 1,
+                title = "Todo 1",
+                description = "Todo 1 description",
+                done = false,
+                hexColor = "#0000FF",
+            )
+        )
+        TodosList(todos = todos)
     }
 }
